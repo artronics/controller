257ba51f04f216ec27e48ae3ca4fa66453480c4e
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-11-12 21:02:41 +0100

    Initial Commit

---
+++ src/main/java/artronics/gsdwn/controller/MainController.java
@@ -0,0 +1,17 @@
+package artronics.gsdwn.controller;
+
+import artronics.chaparMini.Chapar;
+import artronics.chaparMini.exceptions.ChaparConnectionException;
+import artronics.gsdwn.log.Log;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainController
+{
+    public static void main(String[] args){
+
+    }
+    public void start(){
+    }
+}
