eaaf6179620f0ef72c550e3874a49a74002bf647
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-11-17 05:13:21 +0100

    fix the problem with writing log msg twice

--- src/main/java/artronics/gsdwn/log/Log.java
+++ src/main/java/artronics/gsdwn/log/Log.java
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger;
 
 public interface Log
 {
-    Logger MAIN = Logger.getLogger("main");
+    Logger SDWN = Logger.getLogger("sdwn");
     Logger FILE = Logger.getLogger("file");
 
 }
