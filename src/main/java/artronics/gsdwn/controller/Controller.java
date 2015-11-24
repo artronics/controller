ab83529ec3fb89be975285b391a230e704da2460
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-11-24 01:19:52 +0100

    add get acc for queues

--- src/main/java/artronics/gsdwn/controller/Controller.java
+++ src/main/java/artronics/gsdwn/controller/Controller.java
@@ -3,8 +3,14 @@ package artronics.gsdwn.controller;
 import artronics.chaparMini.exceptions.ChaparConnectionException;
 import artronics.gsdwn.packet.Packet;
 
+import java.util.concurrent.BlockingQueue;
+
 public interface Controller
 {
+    BlockingQueue<Packet> getCntRxPacketsQueue();
+
+    BlockingQueue<Packet> getCntTxPacketsQueue();
+
     //TODO change this exp to controller exp
     void start() throws ChaparConnectionException;
 
